82171754dbf9282b5de0c01e789725c1b594205d
Viktor Tisza <vtisza@gmail.com>
2017-02-05 13:06:15 +0100

    Connecting mongodb to store titles

--- predictor.py
+++ predictor.py
@@ -4,6 +4,9 @@ import re
 import numpy as np
 import sys
 from keras.preprocessing.sequence import pad_sequences
+import pymongo
+from pymongo import MongoClient
+import datetime
 
 def predict(title_orig="No title"):
     model_architect_file=open("model/model.json", "r")
@@ -23,4 +26,15 @@ def predict(title_orig="No title"):
     score=model.predict(title)[0][0]
     phrase=score_phrase_dict[min(score_list, key=lambda x:abs(x-score))]
     response= "'{}' would score {:0.1f}. It is {} rating.".format(title_orig,score,phrase)
+    try:
+        mongo_store(title_orig,score,phrase)
+    except:
+        pass
     return(response)
+
+def mongo_store(title_orig,score,phrase):
+    client = MongoClient('mongodb://heroku_app:Heroku2017@ds141209.mlab.com:41209/gametitle')
+    db = client.gametitle
+    name={'title': title_orig, 'score': str(score), 'phrase': phrase, 'time': str(datetime.datetime.now())}
+    db.title.insert_one(name)
+    client.close()
